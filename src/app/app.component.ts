62f9df89840b985f4447b21221978fcb35f79474
mebrihit-z <mebrihit@cxstudios.io>
2025-02-24 10:03:49 -0500

    Adding some components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LeftSidebarComponent } from './components/left-sidebar/left-sidebar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LeftSidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
