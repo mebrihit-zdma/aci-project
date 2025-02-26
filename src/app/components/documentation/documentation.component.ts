439664af563f7d77f6394b2f905a7b670c73f199
mebrihit-z <mebrihit@cxstudios.io>
2025-02-26 18:15:18 -0500

    working on all the dropdown

--- src/app/components/documentation/documentation.component.ts
+++ src/app/components/documentation/documentation.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; 
 
 @Component({
   selector: 'app-documentation',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule ],
   templateUrl: './documentation.component.html',
   styleUrl: './documentation.component.css'
 })
 export class DocumentationComponent {
+  selectedOption1 = '';  
+  selectedOption2 = '';  
 
+  sources = ['JIRA-ADA-516', 'JIRA-ADA-516'];
+  templates = ['User ManuL', 'Release Notes'];
 }
