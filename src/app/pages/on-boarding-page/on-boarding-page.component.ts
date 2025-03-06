0ccf103cd9957cd515b453eb26e6b24a63b5b1dd
mebrihit-z <mebrihit@cxstudios.io>
2025-03-06 16:50:54 -0500

    worked on disable questions section

--- src/app/pages/on-boarding-page/on-boarding-page.component.ts
+++ src/app/pages/on-boarding-page/on-boarding-page.component.ts
@@ -13,7 +13,36 @@ import { Router } from '@angular/router';
 export class OnBoardingPageComponent {
   constructor(private router: Router) {}
 
+  // hide the questions at the beginning 
+  hiddenProductsSection = true;
+  hiddenPersonalizeDashboardSection = true;
+
+  // disable the questions after answered
+  disableRoleSection= false;
+  disableProductsSection = false;
+  disablePersonalizeDashboardSection = false;
+
+  goToRoleSection() {
+    
+  }
+  isActive = false;
+  goToPoductsSection() {
+    this.disableRoleSection = !this.disableRoleSection;
+    this.hiddenProductsSection = !this.hiddenProductsSection; 
+    // this.isActive = !this.isActive;
+  }
+  goToPersonalizeDashboardSection() {
+    this.disableProductsSection = !this.disableProductsSection;
+    this.hiddenPersonalizeDashboardSection = !this.hiddenPersonalizeDashboardSection; 
+  }
+
+
+  goToPervSection() {
+    this.router.navigate(['/dashboard-page']);
+  }
+
   goToDashboard() {
     this.router.navigate(['/dashboard-page']);
   }
+
 }
