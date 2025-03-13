3c7e48da5ff1d78e7a6a97ffe1b0b6ba2691d6b5
mebrihit-z <mebrihit@cxstudios.io>
2025-03-13 09:48:00 -0400

    passing data from the parent

--- src/app/components/cards/summary-card/summary-card.component.ts
+++ src/app/components/cards/summary-card/summary-card.component.ts
@@ -1,9 +1,11 @@
 import { Component,Input  } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; 
 
 @Component({
   selector: 'app-summary-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './summary-card.component.html',
   styleUrl: './summary-card.component.css'
 })
