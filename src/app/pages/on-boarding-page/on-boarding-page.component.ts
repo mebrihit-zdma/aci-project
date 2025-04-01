dd766e2e141402431a027d1f73b84635c55e66a7
mebrihit-z <mebrihit@cxstudios.io>
2025-04-01 14:16:18 -0400

    add card for no option

--- src/app/pages/on-boarding-page/on-boarding-page.component.ts
+++ src/app/pages/on-boarding-page/on-boarding-page.component.ts
@@ -38,6 +38,38 @@ export class OnBoardingPageComponent {
       description :"Monitor the number of bugs raised this month to gauge product stability."
     },
   ]
+  productList  = [
+    {
+      icon:"edit_document",
+      title:"Product Manager",
+      description :"Create, Manage Documentation and product updates."
+    },
+    {
+      icon:"manage_accounts",
+      title:"Help 24 Analyst",
+      description :"Manage Documentation and product updates."
+    },
+    {
+      icon:"manage_accounts",
+      title:"Professional Services",
+      description :"Manage Documentation and product updates."
+    },
+    {
+      icon:"manage_accounts",
+      title:"Solution Architect",
+      description :"Manage Documentation and product updates."
+    },
+    {
+      icon:"wifi_tethering",
+      title:"Sales",
+      description :"Manage Documentation and product updates."
+    },
+    {
+      icon:"edit_document",
+      title:"Marketing",
+      description :"Manage Documentation and product updates."
+    },
+  ]
   // hide the questions at the beginning 
   hiddenProductsSection = true;
   hiddenPersonalizeDashboardSection = true;
