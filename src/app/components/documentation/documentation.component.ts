96985498b926cc79734705c399231f6c10554c5a
mebrihit-z <mebrihit@cxstudios.io>
2025-04-02 17:40:48 -0400

    working on saving the files

--- src/app/components/documentation/documentation.component.ts
+++ src/app/components/documentation/documentation.component.ts
@@ -15,34 +15,47 @@ export class DocumentationComponent {
 
   templates = ['User Manual', 'Release Notes'];
   imagePath ='./app/resources/icons/paste-url-icon.svg';
+
   // sources section
-  // sources = [
-  //   {
-  //     icon:'./app/resources/icons/paste-url-icon.svg',
-  //     source:"JIRA-516: Bug Fixes from latest code chan..."
-  //   },
-  //   {
-  //     icon:'./app/resources/icons/paste-url-icon.svg',
-  //     source:"EPIC-516: Payment Hub Security updates..."
-  //   },
-    
-  // ];
   sources = [
     { source: 'JIRA-516: Bug Fixes from latest code changes' },
     { source: 'EPIC-516: Payment Hub Security updates' }
   ];
 
   newSource: string = '';
-
+  
   addSource() {
     if (this.newSource.trim()) {
       this.sources.push({ source: this.newSource });
       this.newSource = ''; // Clear input after adding
     }
+     // Close the modal
   }
-
+  
   deleteSource(index: number) {
     this.sources.splice(index, 1);
+  }
+  onFileSelected(event: any) {
+    const files = event.target.files;
+    this.handleFiles(files);
+  }
+
+  onDragOver(event: DragEvent) {
+    event.preventDefault();
+  }
+
+  onDrop(event: DragEvent) {
+    event.preventDefault();
+    const files = event.dataTransfer?.files;
+    if (files) {
+      this.handleFiles(files);
+    }
+  }
+
+  handleFiles(files: FileList) {
+    for (let i = 0; i < files.length; i++) {
+      this.sources.push({ source: files[i].name });
+    }
   }
   // tooltip
   skipTooltipValue = false;
