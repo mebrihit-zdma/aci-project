b613f7fba515d4fffabbd72ca8ab608b8f05dd86
mebrihit-z <mebrihit@cxstudios.io>
2025-04-02 20:16:01 -0400

    adeding doc

--- src/app/components/documentation/documentation.component.ts
+++ src/app/components/documentation/documentation.component.ts
@@ -99,22 +99,22 @@ export class DocumentationComponent {
     this.editorTooltipDone = true;
   }
 
-  generatedDocument = [
-    { title: "ACI Payment Gateway – Release Notes (Version 2.5.0)", 
-      releaseDate: "March 15, 2025", 
-      preparedBy: "Product Management Team",
-      overview: "This release introduces enhanced security measures, improved payment processing speed, and new API integrations to streamline bank and   merchant operations. Several bug fixes and performance optimizations have also been included.",
-      newFeaturesAndEnhancements: "FeatureDescriptionImpacted UsersEnhanced Transaction SecurityImplemented multi-layer fraud detection with AI-driven anomaly detection.Bank Operators, Compliance TeamsFaster Payment ProcessingOptimized transaction routing to reduce processing time by 20%.Merchants, IT TeamsNew API for Custom ReportsIntroduced API endpoints for real-time payment tracking and data export.Developers, Business Analysts",
-      bugFixesAndPerformanceImprovements: "IssueResolutionPayment approval delays for high-volume transactions.Improved load balancing and optimized database queries.Incorrect currency conversion in multi-currency transactions.Fixed calculation logic and tested accuracy with various currencies.Help24 system lagging during peak hours.Upgraded infrastructure and optimized query processing.",
-      knownIssuesAndWorkarounds: [
-          "Issue: Some users may experience delays when accessing new API features Workaround: Clear cache or wait for server sync to complete within 5 minutes.",
-          "Issue: Legacy integration users may see warning messages when processing transactions.Workaround: Update to the latest API version or contact support for assistance."
-        ],
-      forFurtherDetailsAndContact: [
-          "ACI Support Team – support@aci.com", 
-        ],
-    },
-  ];
+  // generatedDocument = [
+  //   { title: "Example: ACI Payment Gateway – Release Notes (Version 2.5.0)", 
+  //     releaseDate: "Release Date: March 15, 2025", 
+  //     preparedBy: "Prepared By: Product Management Team",
+  //     overview: "This release introduces enhanced security measures, improved payment processing speed, and new API integrations to streamline bank and   merchant operations. Several bug fixes and performance optimizations have also been included.",
+  //     newFeaturesAndEnhancements: "FeatureDescriptionImpacted UsersEnhanced Transaction SecurityImplemented multi-layer fraud detection with AI-driven anomaly detection.Bank Operators, Compliance TeamsFaster Payment ProcessingOptimized transaction routing to reduce processing time by 20%.Merchants, IT TeamsNew API for Custom ReportsIntroduced API endpoints for real-time payment tracking and data export.Developers, Business Analysts",
+  //     bugFixesAndPerformanceImprovements: "IssueResolutionPayment approval delays for high-volume transactions.Improved load balancing and optimized database queries.Incorrect currency conversion in multi-currency transactions.Fixed calculation logic and tested accuracy with various currencies.Help24 system lagging during peak hours.Upgraded infrastructure and optimized query processing.",
+  //     knownIssuesAndWorkarounds: [
+  //         "Issue: Some users may experience delays when accessing new API features Workaround: Clear cache or wait for server sync to complete within 5 minutes.",
+  //         "Issue: Legacy integration users may see warning messages when processing transactions.Workaround: Update to the latest API version or contact support for assistance."
+  //       ],
+  //     forFurtherDetailsAndContact: [
+  //         "ACI Support Team – support@aci.com", 
+  //       ],
+  //   },
+  // ];
 
   isModalOpen = false; // Initial state (modal is closed)
 
@@ -125,4 +125,28 @@ export class DocumentationComponent {
   closeModal() {
     this.isModalOpen = false;
   }
+
+  generatedDocument =[
+    {
+      title:"1. Overview",
+      description: "This release introduces enhanced security measures, improved payment processing speed, and new API integrations to streamline bank and merchant operations. Several bug fixes and performance optimizations have also been included."
+    },
+    {
+      title: "2. New Features & Enhancements",
+      description:"FeatureDescriptionImpacted UsersEnhanced Transaction SecurityImplemented multi-layer fraud detection with AI-driven anomaly detection.Bank Operators, Compliance TeamsFaster Payment ProcessingOptimized transaction routing to reduce processing time by 20%.Merchants, IT TeamsNew API for Custom ReportsIntroduced API endpoints for real-time payment tracking and data export.Developers, Business Analysts"
+
+    },
+    {
+      title:"3. Bug Fixes & Performance Improvements",
+      description:"IssueResolutionPayment approval delays for high-volume transactions.Improved load balancing and optimized database queries.Incorrect currency conversion in multi-currency transactions.Fixed calculation logic and tested accuracy with various currencies.Help24 system lagging during peak hours.Upgraded infrastructure and optimized query processing."
+    },
+    {
+      title:"4. Known Issues & Workarounds",
+      Issues:
+        [
+          "Issue: Some users may experience delays when accessing new API features.Workaround: Clear cache or wait for server sync to complete within 5 minutes",
+          "Issue: Legacy integration users may see warning messages when processing transactions.Workaround: Update to the latest API version or contact support for assistance."
+        ]
+    },
+  ]
 }
