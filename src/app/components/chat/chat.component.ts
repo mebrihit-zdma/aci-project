69f8e8d1eeb10a0ff1bb30293b8134be47804afc
mebrihit-z <mebrihit@cxstudios.io>
2025-04-15 11:00:03 -0400

    Remove <answer>

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,7 +1,10 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, inject,  OnInit, signal  } from '@angular/core';
+import { Component, inject,  OnInit, signal, Input   } from '@angular/core';
 import { ApiService } from '../../services/api.service';
 import { CommonModule } from '@angular/common';
+
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { marked } from 'marked';
 
 @Component({
   selector: 'app-chat',
@@ -13,8 +16,9 @@ import { CommonModule } from '@angular/common';
 export class ChatComponent implements OnInit {
 
   answer: any = '';
+  safeHtml: SafeHtml = '';
 
-  constructor(private apiService: ApiService){}
+  constructor(private apiService: ApiService, private sanitizer: DomSanitizer){}
 
   ngOnInit(): void {
     this.apiService.get<any>('559041ee-0318-4c65-a185-20d62d735cd7').subscribe({
@@ -22,15 +26,21 @@ export class ChatComponent implements OnInit {
       {
         console.log("api data:", data.chat.answer)
         const raw = data.chat.answer;
-        this.answer = this.extractAnswerText(raw);
+        const extractAnswer = this.extractAnswerText(raw);
+        this.answer = this.convertMarkdown(extractAnswer);
         // this.answer = data.chat.answer;
     },
       error: (err) => console.error('Error:', err),
     });
   }
 
+   // Remove <answer> tags
   extractAnswerText(raw: string): string {
-    // Remove <answer> tags
     return raw.replace(/<\/?answer>/g, '').trim();
+  }
+
+  async convertMarkdown(md: string) {
+    const html = await marked(md || '');
+    this.safeHtml = this.sanitizer.bypassSecurityTrustHtml(html);
   }
 }
