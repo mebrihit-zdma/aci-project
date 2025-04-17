ab52a8da089744f9d87b623519cefdc91c6ae483
mebrihit-z <mebrihit@cxstudios.io>
2025-04-17 10:34:23 -0400

    working on errors

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -15,7 +15,7 @@ import { SourceCardComponent } from '../../components/cards/source-card/source-c
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.css'
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent {
 
   safeHtmlAnswer: SafeHtml = '';
 
@@ -24,22 +24,6 @@ export class ChatComponent implements OnInit {
   sources: { file_name: string, page_number: string, file_path: string }[] = []; 
   
   constructor(private apiService: ApiService, private sanitizer: DomSanitizer){}
-  ngOnInit(): void {
-    // const chat_id = '559041ee-0318-4c65-a185-20d62d735cd7';
-    const chat_id = 'c732cbdd-afdf-4a39-959a-661adc07cb18';
-    this.apiService.get<any>(chat_id).subscribe({
-      next: async (data) => {
-        console.log("api data:", data.chat.answer);
-        const raw = data.chat.answer;
-        const extractAnswer = this.extractAnswerText(raw);
-        this.safeHtmlAnswer = await this.convertMarkdown(extractAnswer);
-        this.question = data.chat.question;
-        this.sources = data.source || [];
-        console.log("this.sources:", this.sources);
-      },
-      error: (err) => console.error('Error:', err),
-    });
-  }
   
   extractAnswerText(raw: string): string {
     const match = raw.match(/<answer>([\s\S]*?)<\/answer>/);
