4bd65fc5c48b563b4380c4ac96b6568745ce102b
mebrihit-z <mebrihit@cxstudios.io>
2025-04-29 19:59:54 -0400

    working on profile

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { MsalService } from '@azure/msal-angular';
+import { AuthenticationResult, AccountInfo } from '@azure/msal-browser';
 
 type ProfileType = {
-  businessPhones: string,
+  businessPhones: string[],
   displayName: string,
   givenName: string,
   jobTitle: string,
@@ -14,7 +16,7 @@ type ProfileType = {
   surname: string,
   userPrincipalName: string,
   id: string
-}
+};
 
 @Component({
   selector: 'app-profile',
@@ -28,12 +30,35 @@ export class ProfileComponent implements OnInit {
   profile!: ProfileType;
   tokenExpiration: string = '';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private msalService: MsalService) {}
 
   ngOnInit() {
-    this.http.get('https://graph.microsoft.com/v1.0/me')
-      .subscribe(profile => {
-        this.profile = profile as ProfileType;
+    const account = this.msalService.instance.getActiveAccount();
+    if (!account) {
+      console.error('No active account found.');
+      return;
+    }
+
+    const tokenRequest = {
+      scopes: ['User.Read'], // ensure this matches your Azure app registration
+      account: account
+    };
+
+    this.msalService.acquireTokenSilent(tokenRequest)
+      .subscribe({
+        next: (result: AuthenticationResult) => {
+          const headers = {
+            Authorization: `Bearer ${result.accessToken}`
+          };
+
+          this.http.get<ProfileType>('https://graph.microsoft.com/v1.0/me', { headers })
+            .subscribe(profile => {
+              this.profile = profile;
+            });
+        },
+        error: (err: any) => {
+          console.error('Token acquisition failed:', err);
+        }
       });
 
     this.tokenExpiration = localStorage.getItem('tokenExpiration') ?? '';
