834929057c7ad6d389e1085bf926c932fd1c37d7
mebrihit-z <mebrihit@cxstudios.io>
2025-05-05 18:34:52 -0400

    session id created

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -11,6 +11,7 @@ import { SourceCardComponent } from '../../components/cards/source-card/source-c
 import { AnswerSource, ChatMessage } from '../../models/chat.model';
 import { extractAnswerText, convertMarkdown, extractSources } from '../../utils/chat-utils';
 import { UserService } from '../../services/user.service';
+import { firstValueFrom } from 'rxjs';
 
 
 
@@ -26,6 +27,8 @@ export class ChatComponent {
   app_id = "67daf330d62c5ade928150d1";
   model_name ="openai/gpt-4o";
   top_k = 3;
+
+  sessionId: any = "";
 
   askedQuestion: string = '';
   sources: AnswerSource[] = [];
@@ -93,31 +96,21 @@ export class ChatComponent {
     });
   }
   
-  // Create a session
-  // createSession(userId: string) {
-  //   console.log("userId: ", userId)
-  //   const payload = {
-  //     user_id: userId,
-  //     app_id: "string"
-  //   };
-  //   this.apiService.post<any>('chat_stream', payload, 'text').subscribe({
-  //     next: async (data) => {
-  //       console.log("api post data mz:", data);
-  //       const extractAnswer = extractAnswerText(data);
-  //       const safeAnswer = await convertMarkdown(extractAnswer, this.sanitizer);
-        
-  //       let answerSource: AnswerSource[] = extractSources(data); 
-        
-  //       this.messages.push({ sender: 'user', text: askedQuestion });
-  //       this.messages.push({ 
-  //         sender: 'bot', 
-  //         text: safeAnswer, 
-  //         sources: answerSource 
-  //        });
-  //     },
-  //     error: (err) => console.error('Error:', err),
-  //   });
-  // }
+  // Create a session id using user id
+  createSessionId(userId: any) {
+    const payload = {
+      user_id: userId,
+      app_id: this.app_id
+    };
+    this.apiService.post<any>('create_session', payload, 'json').subscribe({
+      next: async (data) => {
+        console.log('createSession data:', data);
+        console.log('session_id:', data?.session_id);
+        this.sessionId = data?.session_id;
+      },
+      error: (err) => console.error('Error:', err),
+    });
+  }
   ngOnInit() {
     // selected question from chat history
     this.chatService.click$.subscribe(() => {
@@ -127,6 +120,8 @@ export class ChatComponent {
     this.chatService.startNewChatClick$.subscribe(() => {
       this.sources = [];
       this.messages = [];
+      this.createSessionId(this.userService.getUserId());
+      console.log("sessionId: ", this.sessionId)
     });
   }
 
