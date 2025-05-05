d22f487266989f59ebcbca564a30a035310e9520
mebrihit-z <mebrihit@cxstudios.io>
2025-05-05 16:23:17 -0400

    adding user role

--- src/app/msal-config.ts
+++ src/app/msal-config.ts
@@ -8,6 +8,9 @@ export function msalInstanceFactory() {
       clientId: '9298e23f-f951-4164-ad53-92347706120b',
       authority: 'https://login.microsoftonline.com/45488ed5-ce1d-4d00-87d2-144f056d15d3',
       redirectUri: 'http://localhost:4200',
+      // clientId: '0071edf4-4545-4797-81ea-c5ef43041030',
+      // authority: 'https://login.microsoftonline.com/45488ed5-ce1d-4d00-87d2-144f056d15d3',
+      // redirectUri: 'https://d15d4x71dime5a.cloudfront.net',
     },
     cache: {
       cacheLocation: 'localStorage',
